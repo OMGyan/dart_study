1f737f9684b2452389392c5320b96c78c426934a
yanxun <1768695841@qq.com>
2019-05-23 16:35:50 +0800

    dart 可变参数方法定义示例

--- chapter5/function_optional_param.dart
+++ chapter5/function_optional_param.dart
@@ -11,9 +11,17 @@ void main(){
    printPerson2('kobe');
    printPerson2('kobe',38);
    printPerson2('kobe',38,'man');
+   
+   /**
+   * 可变参数添加默认值
+   */
+   printPerson3('杜兰特');
+   printPerson3('杜兰特',age:34,sex:'women');
 }
 
 printPerson(String name,{int age,String sex}) => print('name:$name,age:$age,sex:$sex');
     
 printPerson2(String name,[int age,String sex]) => print('name:$name,age:$age,sex:$sex');
+
+printPerson3(String name,{int age = 30,String sex = 'man'}) => print('name:$name,age:$age,sex:$sex');
 
