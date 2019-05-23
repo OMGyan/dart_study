e4d0146a6bf25b2c01debf016b5d8b31370d220b
yanxun <1768695841@qq.com>
2019-05-23 10:18:40 +0800

    dart逻辑运算符示例

---
+++ chapter3/operator_logical.dart
@@ -0,0 +1,12 @@
+void main(){
+  //逻辑运算符示例
+  bool isTrue = true;
+  print(!isTrue);
+
+  bool isFalse = false;
+  print(isTrue && isFalse);
+  print(isTrue || isFalse);
+
+  String str = "";
+  print(!str.isEmpty);
+}
