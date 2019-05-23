1b49be41cb3fb20e355ca759ab1375507eb9fd67
yanxun <1768695841@qq.com>
2019-05-23 12:00:30 +0800

    dartfor循环语句示例

---
+++ chapter4/stmt_for.dart
@@ -0,0 +1,13 @@
+//for循环语句示例
+void main(){
+  var list = [23,'j',24,'k'];
+  for(var i = 0;i < list.length;i++){
+    print(list[i]);
+  }
+
+  print('-------------------------------------');
+
+  for (var item in list) {
+    print(item);
+  }
+}
